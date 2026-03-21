f298037dc4a6abf4e0c4833467c85d301ec9b7f9
mirr-x <amir.exe@icloud.com>
2026-03-21 02:01:40 +0000

    Add maze generation functionality and project structure
    - Implement maze generation logic in `maze.py`
    - Create drawing functionality in `draw_maze.py`
    - Define project phases and run checklist in `PLAN.md`
    - Establish type definitions in `types.py`
    - Initialize package structure in `__init__.py`
    - Add README file for project documentation

---
+++ amazeing/draw_maze.py
@@ -0,0 +1,59 @@
+from typing import Protocol
+
+import os
+
+from .types import Coord
+
+
+class MazeLike(Protocol):
+    width: int
+    height: int
+    grid: list[list[dict[str, bool]]]
+
+
+class DrawMaze:
+    def __init__(self, maze: MazeLike) -> None:
+        self.maze = maze
+
+    def _print_roof(self, cells: int, row: int) -> None:
+        for i in range(cells):
+            if self.maze.grid[row][i]["N"]:
+                print("+---", end="")
+            else:
+                print("+   ", end="")
+        print("+")
+
+    def _print_walls(self, cells: int, row: int, current_cell: Coord) -> None:
+        for i in range(cells):
+            if self.maze.grid[row][i]["W"]:
+                print("|", end="")
+            else:
+                print(" ", end="")
+            if (row, i) == current_cell:
+                print(" * ", end="")
+            elif self.maze.grid[row][i]["visited"]:
+                print(" . ", end="")
+            else:
+                print("   ", end="")
+        if self.maze.grid[row][cells - 1]["E"]:
+            print("|")
+        else:
+            print(" ")
+
+    def _print_last_ground(self, cells: int, row: int):
+        for i in range(cells):
+            if self.maze.grid[row][i]["S"]:
+                print("+---", end="")
+            else:
+                print("+   ", end="")
+        print("+")
+
+    def draw_maze(self, current_cell: Coord) -> None:
+        os.system("clear")
+        width = self.maze.width
+        height = self.maze.height
+        for row in range(height):
+            self._print_roof(width, row)
+            self._print_walls(width, row, current_cell)
+            if row == height - 1:
+                self._print_last_ground(width, row)
